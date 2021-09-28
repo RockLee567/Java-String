af848e567298f74f6871f3bffef94d8110b9d04e
Tung <hstung12a1@gmail.com>
2021-09-28 17:34:41 +0700

    So sanh String     Ko phan biet Lower Upper

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,11 @@
+public class equalsIgnoreCase {
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+        String s3 = "JAVA";
+        String s4 = "python";
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s1.equalsIgnoreCase(s4));
+    }
+}
