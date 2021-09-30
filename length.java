e9e201870002234907dae8b81a3236d802dcbdd2
Tung <hstung12a1@gmail.com>
2021-09-30 18:00:03 +0700

    Do dai ky tu(+space) --- trong --- String

---
+++ length.java
@@ -0,0 +1,8 @@
+public class length {
+    public static void main(String[] args) {
+        String a = "Codelearn" ;              // => 9
+        String b = "Code Geass" ;             // => 10 
+        System.out.println( a.length() );
+        System.out.println( b.length() );
+    }
+}
