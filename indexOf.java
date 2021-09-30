8f959a60ff0e9f60d1f48ebc7e037b820543ed62
Tung <hstung12a1@gmail.com>
2021-09-30 16:57:52 +0700

    Vi tri cua String

---
+++ indexOf.java
@@ -0,0 +1,26 @@
+public class indexOf {
+    public static void main(String[] args) {
+        String s1 = "This is index of example" ; 
+
+        int wStringOne = s1.indexOf("is")  ;           // => 2 
+        int wStringTwo = s1.indexOf( "index" ) ;       // => 8 
+        System.out.println( wStringOne );           
+        System.out.println( wStringTwo ); 
+
+        int wStringThree = s1.indexOf( "is" , 4) ;     // => 5 
+        int wCharOne = s1.indexOf( 's' ) ;             // => 3 
+        System.out.println( wStringThree ); 
+        System.out.println( wCharOne );   
+        
+
+        int a = s1.indexOf( 8 ) ; 
+        System.out.println( a ) ;                      // => -1 
+
+        /*
+            int indexOf( String sub  )     
+            int indexOf( String sub , int fromIndex ) 
+            int indexOf( char star )      
+        */ 
+
+    }
+}
