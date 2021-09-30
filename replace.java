1e6c83869350e0b945379de3c1a3f0c78f9a9466
Tung <hstung12a1@gmail.com>
2021-09-30 18:09:05 +0700

    Thay The--- 1 Chu -> 1 Chu Moi

---
+++ replace.java
@@ -0,0 +1,9 @@
+public class replace {
+    public static void main(String[] args) {
+        String a = "He is a good boy" ; 
+        String b = a.replace("good" , "bad") ; 
+        System.out.println( b );
+
+        // replace thay the chuoi,ky tu thanh chuoi , ky tu khas 
+    }
+}
