f80792c48ef24af450eaf7b96222b382aed15f58
Tung <hstung12a1@gmail.com>
2021-10-01 13:16:54 +0700

    in 1 chuoi trong 1 String Big = chi so cua Vi Tri can In

---
+++ substring.java
@@ -0,0 +1,11 @@
+public class substring {
+    /*
+        public String substring(int startIndex) 
+        public String substring(int startIndex, int endIndex)  
+    */ 
+    public static void main(String[] args) {
+        String s1 = "hellojava";
+        System.out.println(s1.substring(3, 7));// "loja"  
+        System.out.println(s1.substring(3));// "lojava" 
+    }
+}
